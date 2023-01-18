203bd890cd2d44f62cc6abedef8c29ac4fc69917
Zachary Cygan <zachcygan@gmail.com>
2023-01-18 13:37:07 -0800

    started styling webpage

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var optionOne = document.createElement(`li`);
+var optionTwo = document.createElement(`li`);
+var optionThree = document.createElement(`li`);
+var optionFour = document.createElement(`li`)
