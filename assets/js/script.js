89c26e77ce4177ce475a13306a4c392a76d1c3fb
Zachary Cygan <zachcygan@gmail.com>
2023-01-18 19:28:39 -0800

    added countdown timer when clicking the start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
-var optionOne = document.createElement(`li`);
-var optionTwo = document.createElement(`li`);
-var optionThree = document.createElement(`li`);
-var optionFour = document.createElement(`li`)
+var highScores = document.getElementById(`highScores`);
+var score = document.getElementById(`score`);
+var start = document.getElementById(`start`);
+var time = document.getElementById(`time`);
+
+time.textContent = `Time: `
+
+start.addEventListener(`click`, function() {
+    var timeRemaining = 60;
+    
+    var timeInterval = setInterval(function() {
+        if (timeRemaining > 1) {
+            time.textContent = `Time: ` + timeRemaining;
+            timeRemaining--;
+        } else {
+            time.textContent = `Time: 0`;
+            clearInterval(timeInterval);
+        }
+    }, 1000)
+
+    return time;
+});
+
