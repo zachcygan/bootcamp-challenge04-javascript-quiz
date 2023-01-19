2c04fc90ca830196b92ff7d20b2952e98ab51e86
Zachary Cygan <zachcygan@gmail.com>
2023-01-19 11:21:03 -0800

    changed start button to start/stop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,32 @@
 var highScores = document.getElementById(`highScores`);
 var score = document.getElementById(`score`);
-var start = document.getElementById(`start`);
 var time = document.getElementById(`time`);
+var start = document.getElementById(`start`);
+var stop = document.getElementById(`stop`);
+var reset = document.getElementById(`reset`);
 var questions = document.getElementById(`questions`);
 var choice1 = document.getElementById(`choice1`);
 
-
+var timeRemaining = 60;
 time.textContent = `Time: `;
 questions.textContent = `When you are ready, click the start button to begin.`
-choice1.textContent = `test`
 
-start.addEventListener(`click`, function() {
-    var timeRemaining = 60;
-    
-    var timeInterval = setInterval(function() {
-        if (timeRemaining > 1) {
-            time.textContent = `Time: ` + timeRemaining;
-            timeRemaining--;
-        } else {
-            time.textContent = `Time: 0`;
-            clearInterval(timeInterval);
-        }
-    }, 1000)
+// start.addEventListener(`click`, function() {
+//     start.dissabled = true;
+//     var timeInterval = setInterval(function() {
+//         if (timeRemaining > 1) {
+//             time.textContent = `Time: ` + timeRemaining;
+//             timeRemaining--;
+//         } else {
+//             time.textContent = `Time: 0`;
+//             clearInterval(timeInterval);
+//         }
+//     }, 1000)
+// });
 
-    return time;
-});
-
+// stop.addEventListener(`click`, function() {
+//     var timeInterval = setInterval(function() {
+//         time.textContent = `Time: ` + timeRemaining;
+//         clearInterval(timeInterval)
+//     })
+// })
