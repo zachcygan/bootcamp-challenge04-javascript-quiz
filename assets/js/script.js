5ece75e609773745e04b00a32d86740ca4571838
Zachary Cygan <zachcygan@gmail.com>
2023-01-18 19:35:38 -0800

    final commit of night for save

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,13 @@ var highScores = document.getElementById(`highScores`);
 var score = document.getElementById(`score`);
 var start = document.getElementById(`start`);
 var time = document.getElementById(`time`);
+var questions = document.getElementById(`questions`);
+var choice1 = document.getElementById(`choice1`);
 
-time.textContent = `Time: `
+
+time.textContent = `Time: `;
+questions.textContent = `When you are ready, click the start button to begin.`
+choice1.textContent = `test`
 
 start.addEventListener(`click`, function() {
     var timeRemaining = 60;
