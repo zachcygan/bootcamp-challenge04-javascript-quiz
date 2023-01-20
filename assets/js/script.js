274ba826c2c2f93c26b89aeddf11dee83d8ab650
Zachary Cygan <zachcygan@gmail.com>
2023-01-20 15:00:55 -0800

    working on setting correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var startButton = document.getElementById(`startButton`);
 var stopButton = document.getElementById(`stopButton`);
 var reset = document.getElementById(`reset`);
 var questionsArea = document.getElementById(`questions`);
+var answerContainer = document.querySelector(`.answerContainer`);
 var playing = false;
 
 var timeRemaining = 60;
@@ -14,6 +15,7 @@ var timeInterval;
 startButton.addEventListener('click', startTimer);
 stopButton.addEventListener('click', stopTimer);
 reset.addEventListener(`click`, resetTimer);
+answerContainer.addEventListener(`click`, nextQuestion);
 
 var questions = [
     {
@@ -53,6 +55,12 @@ var questions = [
     },
 ]
 
+// correctAnswers stores the correct answer from each question
+var correctAnswers = [];
+for (var i = 0; i < questions.length; i++) {
+    correctAnswers.push(questions[i].answer)
+}
+
 var choice1 = document.getElementById(`choice1`);
 var choice2 = document.getElementById(`choice2`);
 var choice3 = document.getElementById(`choice3`);
@@ -63,11 +71,15 @@ var score = 0;
 function displayQuestions(index) {
     if (index < questions.length) {
         questionsArea.textContent = questions[index].question;
+        choice1.textContent = questions[index].choice[1];
+        choice2.textContent = questions[index].choice[2];
+        choice3.textContent = questions[index].choice[3];
+        choice4.textContent = questions[index].choice[4];
     }
-    choice1.textContent = questions[index].choice[1];
-    choice2.textContent = questions[index].choice[2];
-    choice3.textContent = questions[index].choice[3];
-    choice4.textContent = questions[index].choice[4];
+
+    if (index > questions.length) {
+        clearInterval(timeInterval);
+    }
 }
 
 // if event.target.value() === questions[index].answer
@@ -86,7 +98,7 @@ function startTimer() {
     // start.disabled = true;
 	}, 1000);
     playing = true;
-    displayQuestions(index);
+    displayQuestions(0);
 };
 
 function stopTimer() {
@@ -98,49 +110,39 @@ function resetTimer() {
     questionsArea.textContent = `When you are ready, click the start button to begin.`;
     clearInterval(timeInterval);
     timeRemaining = 60;
-    time.textContent = `Time: 60`
-    // need to include reseting the game as well, not just the timer
+    time.textContent = `Time: 60`;
     choice1.textContent = ``;
     choice2.textContent = ``;
     choice3.textContent = ``; 
     choice4.textContent = ``;
-    scoreEl.textContent = `Score:` 
+    scoreEl.textContent = `Score:`;
+    index = 0;
 }
 
-// correctAnswers stores the correct answer from each question
-var correctAnswers = [];
-for (var i = 0; i < questions.length; i++) {
-    correctAnswers.push(questions[i].answer)
-}
-console.log(correctAnswers)
-
-var answerContainer = document.querySelector(`.answerContainer`);
-answerContainer.addEventListener(`click`, nextQuestion);
 function nextQuestion(event) {
     if (playing) {
         var userChoice = event.target;
-        console.log(userChoice)
-        console.log(userChoice.value)
-        console.log(questions[0].answer.value)
+        console.log(typeof userChoice.textContent)
+        console.log(typeof questions[0].answer)
+        console.log(score)
 
         if (userChoice.matches('.choices')) {
             index++;
             displayQuestions(index);
-        } else if (userChoice.value === questions[index].answer.value) {
-        score++;
-        scoreEl.textContent = `Score: ${score}`
+        } else if (userChoice.textContent === questions[index].answer.textContent) {
+            score++;
+            scoreEl.textContent = `Score: ${score}`
         }
     }
 
-    if (index === questions.length) {
-        var userChoice = event.target;
-
-        if (userChoice.matches(`.choices`)) {
-            questionsArea.textContent = `Done! Your final score is ${score}/3`;
-            choice1.textContent = ``;
-            choice2.textContent = ``;
-            choice3.textContent = ``; 
-            choice4.textContent = ``;
-        } 
+    if (index > questions.length) {
+        questionsArea.textContent = `Done! Your final score is ${score}/3`;
+        choice1.textContent = ``;
+        choice2.textContent = ``;
+        choice3.textContent = ``; 
+        choice4.textContent = ``;
     }
 }
+
+
+
